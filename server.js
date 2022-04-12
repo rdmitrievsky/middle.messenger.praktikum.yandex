54be176eb01e2e5a62242ec5644181cadc96019f
rdmitrievsky <rdmitrievsky@gmail.com>
2022-04-13 02:35:09 +0300

    asd

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('./', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'))
-})
+// app.use(express.static(path.join(__dirname, 'dist')));
+// app.get('./', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'dist/index.html'))
+// })
+app.use(express.static('dist'))
 
 app.listen(port, () => {
     console.log(`oprt is ${port}`)
